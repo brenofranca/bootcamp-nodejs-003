3ae77ae8baaa63e35552e032717ce1d7d68c8e02
Breno França <franciscobreno.si@gmail.com>
2019-01-06 14:18:27 -0300

    database - configuracao do mongoose

--- src/bootstrap/server.js
+++ src/bootstrap/server.js
@@ -1,26 +1,42 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const databaseConfig = require('../config/database')
 
-const routes = require("../app/routes/index");
+const routes = require('../app/routes/index')
 
 class App {
-  constructor() {
-    this.express = express();
+  constructor () {
+    this.express = express()
 
-    this.isDEV = process.env.NODE_ENV !== "production";
+    this.isDEV = process.env.NODE_ENV !== 'production'
 
-    this.middlewares();
+    this.middlewares()
 
-    this.routes();
+    this.database()
+
+    this.routes()
   }
 
-  middlewares() {
-    this.express.use(bodyParser.json());
+  middlewares () {
+    this.express.use(bodyParser.json())
+
+    this.express.use(bodyParser.urlencoded({ extended: true }))
   }
 
-  routes() {
-    this.express.use(routes);
+  database () {
+    mongoose.connect(
+      databaseConfig.mongo.uri,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true
+      }
+    )
+  }
+
+  routes () {
+    this.express.use(routes)
   }
 }
 
-module.exports = new App().express;
+module.exports = new App().express
