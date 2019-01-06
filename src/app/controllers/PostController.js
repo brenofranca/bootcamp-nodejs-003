a9842b50174f83741c8c805ea85bf079b18df43d
Breno França <franciscobreno.si@gmail.com>
2019-01-06 15:20:36 -0300

    recourse_posts - adicionado o crud de posts de um author

---
+++ src/app/controllers/PostController.js
@@ -0,0 +1,37 @@
+const Post = require('../models/Post')
+
+class PostController {
+  async index (req, res) {
+    const posts = await Post.find()
+
+    return res.json(posts)
+  }
+
+  async show (req, res) {
+    const post = await Post.findById(req.params.id)
+
+    return res.json(post)
+  }
+
+  async store (req, res) {
+    const post = await Post.create({ ...req.body, author: req.userId })
+
+    return res.json(post)
+  }
+
+  async update (req, res) {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+
+    return res.json(post)
+  }
+
+  async destroy (req, res) {
+    await Post.findByIdAndDelete(req.params.id)
+
+    return res.send()
+  }
+}
+
+module.exports = new PostController()
--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -3,11 +3,22 @@ const express = require('express')
 const router = express.Router()
 
 const controllers = require('../controllers')
-const middlewares = require('../middlewares')
+const authMiddleware = require('../middlewares/auth')
 
 router.get('/', (req, res) => res.send('API Only!'))
 
 router.post('/users', controllers.UserController.store)
 router.post('/sessions', controllers.SessionController.store)
+
+router.use(authMiddleware)
+
+/**
+ *  Post REST API
+ */
+router.get('/posts', controllers.PostController.index)
+router.get('/posts/:id', controllers.PostController.show)
+router.post('/posts', controllers.PostController.store)
+router.put('/posts/:id', controllers.PostController.update)
+router.delete('/posts/:id', controllers.PostController.destroy)
 
 module.exports = router
