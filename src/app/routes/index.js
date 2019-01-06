a42c0b090a18811382e095cc93a57a1a02321601
Breno França <franciscobreno.si@gmail.com>
2019-01-06 14:46:05 -0300

    user_auth - adicionado o recurso de gerar o token do usuario que esta logando

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -3,9 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 const UserController = require('../controllers/UserController')
+const SessionController = require('../controllers/SessionController')
 
 router.get('/', (req, res) => res.send('API Only!'))
 
 router.post('/users', UserController.store)
+router.post('/sessions', SessionController.store)
 
 module.exports = router
