faab1a27cab9d323112257bb93013ba92845f6d4
Breno França <franciscobreno.si@gmail.com>
2019-01-07 12:43:31 -0300

    resources_validators - adicionado as validacoes nos inputs

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,29 +1,51 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const router = express.Router()
 
 const controllers = require('../controllers')
-const authMiddleware = require('../middlewares/auth')
+const validators = require('../validators')
+const middlewares = require('../middlewares')
 
 router.get('/', (req, res) => res.send('API Only!'))
 
-router.post('/users', controllers.UserController.store)
-router.post('/sessions', controllers.SessionController.store)
+router.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+router.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
-router.use(authMiddleware)
+router.use(middlewares.Auth)
 
 /**
  *  Post REST API
  */
 router.get('/posts', controllers.PostController.index)
 router.get('/posts/:id', controllers.PostController.show)
-router.post('/posts', controllers.PostController.store)
-router.put('/posts/:id', controllers.PostController.update)
+router.post(
+  '/posts',
+  validate(validators.Post),
+  controllers.PostController.store
+)
+router.put(
+  '/posts/:id',
+  validate(validators.Post),
+  controllers.PostController.update
+)
 router.delete('/posts/:id', controllers.PostController.destroy)
 
 /**
  *  Purchase Post REST API
  */
-router.post('/purchases', controllers.PurchaseController.store)
+router.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = router
